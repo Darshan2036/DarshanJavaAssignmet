c853621a3d4f471767766a36146a8b1ef53638b2
Darshan2036 <darshanindore999@gmail.com>
2025-07-03 18:21:49 +0530

    Create test.java

---
+++ test.java
@@ -0,0 +1,32 @@
+public class test {
+    public static void main(String[] args) {
+
+        
+        int age = 19; // Integer data type
+        // This is a comment explaining the variable
+
+        
+        float temperature = 36.5f; // Float data type, 'f' is used to denote float literals
+        // This is a comment explaining the variable
+
+        
+        double pi = 3.14159;// Double data type, more precision than float
+
+        
+        char grade = 'B';// Character data type, single character enclosed in single quotes
+
+        
+        boolean isPassed = true;// Boolean data type, can be true or false
+
+        
+        String name = "Avishkar";// String data type, sequence of characters enclosed in double quotes
+
+        
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Temperature: " + temperature);
+        System.out.println("Value of Pi: " + pi);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+    }
+}
